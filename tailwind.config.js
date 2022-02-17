21cd583ba1a0f4d18467138eb5fedb790b33ffc8
Agnel Nieves <agnelnieves@gmail.com>
2022-02-17 17:54:17 -0500

    feat: add tailwind theming

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        'nuxty-dark': '#00502F',
+        'nuxty-darker': '#012A35',
+        'nuxty-darkest': '#001A21',
+        'nuxty-primary': '#32F0A2',
+      },
+    },
+  },
+}
