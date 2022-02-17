9d9e4607a8b83a2f463c8da8be4b9b842006f22c
Agnel Nieves <agnel.nievesfuentes@ukg.com>
2022-02-17 19:53:32 +0000

    feat: add Next Previous component

---
+++ components/NextPrev.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="flex justify-center md:justify-between flex-wrap">
+    <NuxtLink
+      v-if="data.prev"
+      :to="{ name: 'blog-slug', params: { slug: data.prev.slug } }"
+      class="font-bold hover:underline prev_next--link flex"
+    >
+      <div class="flex justify-between">
+        <div class="py-6 pl-6">
+          <p class="text-sm opacity-80">prev post</p>
+          {{ data.prev.title }}
+        </div>
+        <img
+          class="ml-6 object-cover w-24 h-full"
+          :src="data.prev.image.src"
+          :alt="data.prev.image.alt"
+        />
+      </div>
+    </NuxtLink>
+    <span v-else>&nbsp;</span>
+    <NuxtLink
+      v-if="data.next"
+      :to="{ name: 'blog-slug', params: { slug: data.next.slug } }"
+      class="font-bold hover:underline prev_next--link flex"
+    >
+      <div class="flex justify-between">
+        <img
+          class="mr-6 object-cover w-24 h-full"
+          :src="data.next.image.src"
+          :alt="data.next.image.alt"
+        />
+        <div class="py-6 pr-6">
+          <p class="text-sm opacity-80">Next post</p>
+          {{ data.next.title }}
+        </div>
+      </div>
+    </NuxtLink>
+    <span v-else>&nbsp;</span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    data: {
+      type: Object,
+      required: true,
+      default: () => null,
+    },
+  },
+}
+</script>
+
+<style lang="scss">
+.prev_next--link {
+  @apply my-3 bg-gray-200;
+}
+</style>
--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -16,6 +16,9 @@
     <article class="article">
       <nuxt-content :document="article" />
     </article>
+    <div class="next-prev-container">
+      <NextPrev v-if="articles.prev || articles.next" :data="articles" />
+    </div>
   </div>
 </template>
 
@@ -26,8 +29,15 @@ export default {
   async asyncData({ $content, params }) {
     // getting the article's data from the params
     const article = await $content('articles', params.slug).fetch()
+
+    const [prev, next] = await $content('articles')
+      .only(['title', 'slug', 'image', 'alt']) // only need title and slug
+      .sortBy('createdAt', 'asc') // sorting by the creation date
+      .surround(params.slug)
+      .fetch()
     return {
       article,
+      articles: { prev, next },
     }
   },
   methods: {
@@ -36,7 +46,7 @@ export default {
 }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
 .article {
   @apply 2xl:max-w-6xl xl:max-w-4xl lg:max-w-2xl md:max-w-lg sm:max-w-sm max-w-full mx-auto px-6 text-black my-10 flex;
 }
@@ -52,9 +62,13 @@ h1 {
 .main-img {
   @apply rounded-xl mt-5 2xl:max-w-7xl xl:max-w-5xl lg:max-w-4xl md:max-w-2xl sm:max-w-lg max-w-full mx-auto px-4;
 }
+
+.next-prev-container {
+  @apply 2xl:max-w-6xl xl:max-w-4xl lg:max-w-2xl md:max-w-lg sm:max-w-sm max-w-full mx-auto;
+}
 </style>
 
-<style>
+<style lang="scss">
 .article * {
   @apply max-w-full;
 }
