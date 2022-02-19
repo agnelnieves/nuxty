8de049d753c512772812b2904fab83c2a00a456e
Agnel Nieves <agnelnieves@gmail.com>
2022-02-19 13:25:52 -0500

    feat: add favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,9 +10,29 @@ export default {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
       { name: 'format-detection', content: 'telephone=no' },
+      { name: 'msapplication-TileColor', content: '#da532c' },
+      { name: 'theme-color', content: '#012A35' },
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: 'apple-touch-icon',
+        sizes: '180x180',
+        href: '/apple-touch-icon.png',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '32x32',
+        href: '/favicon-32x32.png',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '16x16',
+        href: '/favicon-16x16.png',
+      },
+      { rel: 'manifest', href: '/site.webmanifest' },
+      { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#012A35' },
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
       // --- GOOGLE FONTS ---
       { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
